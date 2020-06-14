4465365f049a5e9dd413372146f6b50669108831
anonimQwerty <61067633+anonimQwerty@users.noreply.github.com>
2020-06-14 20:10:55 +0300

    Add files via upload

---
+++ translate.py
@@ -0,0 +1,18 @@
+russian={
+	'input_file':'Имя входного файла с .txt: ',
+	'output_file':'Имя файла с выходными данными с .txt: ',
+	'answer':'Сохранять как библиотеку питона? y/n: ',
+	'name':'Имя с .py: ',
+	'threads':'Потоки число: ',
+	'good':' хороший',
+	'bad':' плохой'
+	}
+eng={
+	'input_file':'Name input file with .txt: ',
+	'output_file':'Name file with exist data with .txt: ',
+	'answer':'Save as python lib? y/n: ',
+	'name':'name with .py: ',
+	'threads':'Threads number: ',
+	'good':' good',
+	'bad':' bad'
+	}
